4e53f9049431eed716767a0421478ada2e6d6fce
Kirillov Alexander <kirillovbrest@gmail.com>
2021-07-06 10:13:22 +0300

    Security updated

--- src/main/java/ru/netology/netologyjdbctask/configuration/WebSecurityConfig.java
+++ src/main/java/ru/netology/netologyjdbctask/configuration/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package ru.netology.netologyjdbctask.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -8,10 +9,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+    }
+
+    @Override
     public void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .and()
                 .authorizeRequests().antMatchers("/hello").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().hasRole("ADMIN");
     }
 }
